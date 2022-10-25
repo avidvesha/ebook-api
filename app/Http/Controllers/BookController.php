5dca14050c42b63558cd99b8c6d1c17aaa5f5e15
avidvesha <apit.scorpio10@gmail.com>
2022-10-25 15:32:33 +0700

    laravel 3 update

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Book;
+
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Book::all();
+
+        return response()->json([
+            "message"=>"Load Data Book Success!",
+            "book"=>$data
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $table = Book::create([
+            "book" => $request->book,
+            "author" => $request->author,
+            "year" => $request->year
+        ]);
+
+        return response()->json([
+            "message"=>"Store Book Success!",
+            "book"=>$table
+        ], 201);
+}
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $table = Book::find($id);
+        if($table){
+            return $table;
+        }else{
+            return ["message" => "Data Book Not Found!"];
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $table = Book::find($id);
+        if($table){
+            $table->book = $request->book ? $request->book : $table->book;
+            $table->author = $request->author ? $request->author : $table->author;
+            $table->year = $request->year ? $request->year : $table->year;
+            $table->save();
+
+            return $table;
+        }else{
+            return ["message"=>"Data Book Not Found!"];
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $table = Book::find($id);
+        if($table){
+            $table->delete();
+
+            return ["message"=>"Delete Book Success!"]; 
+        }else{
+            return ["message"=>"Data Book Not Found!"];
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HeloController;
+use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\BookController;
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +19,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Route::get('halo', function(){
+//     return ['me' => 'Gamteng'];
+// });
+
+// Route::get('siswa', [SiswaController::Class, 'index']);
+
+// Route::get('siswa/{id}', [SiswaController::Class, 'show']);
+
+// Route::post('siswa', [SiswaController::Class, 'store']);
+
+// Route::put('siswa/{id}', [SiswaController::Class, 'update']);
+
+// Route::delete('siswa/{id}', [SiswaController::Class, 'destroy']);
+
+Route::resource('siswa', SiswaController::class);
+Route::resource('halo', HeloController::class);
+Route::resource('books', BookController::class);
+
