8ae0add483906c4884dc731aa6a966a9f21e3aa9
avidvesha <apit.scorpio10@gmail.com>
2022-11-01 15:53:05 +0700

    laravel 4 auth

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SiswaController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +16,18 @@ use App\Http\Controllers\BookController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::middleware('auth:sanctum')->get('/user', function(Request $request) {
+  return $request->user();
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
 
 Route::resource('siswa', SiswaController::class);
-Route::resource('books', BookController::class);
+Route::resource('books', BookController::class)->except('store', 'update');
 
+Route::middleware('auth:sanctum')->group(function() {
+  Route::resource('books', BookController::class)->except('create', 'edit', 'show', 'index');
+  Route::post('/logout', [AuthController::class, 'logout']);
+});
